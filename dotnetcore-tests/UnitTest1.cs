3a3e390b8cfed7f468669d686a9da38c479bcf6a
Sutapa Kundu <myWiproSutapa@outlook.com>
2019-08-05 10:16:22 +0000

    test

--- dotnetcore-tests/UnitTest1.cs
+++ dotnetcore-tests/UnitTest1.cs
@@ -18,7 +18,7 @@ namespace dotnetcore_tests
           ViewResult result = controller.About() as ViewResult;
 
           // Assert
-          Assert.AreEqual("Your application description page.", result.ViewData["Message"]);
+          Assert.AreEqual("Your application description page 444.", result.ViewData["Message"]);
         }
 
         [TestMethod]
