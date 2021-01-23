41fd53d71b6818282adb9ebc52e1619904545d15
vibhor-voicebot <53684148+vibhor-voicebot@users.noreply.github.com>
2021-01-23 10:56:49 -0600

    AB#1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace pipelines_dotnet_core.Controllers
         }
 
 
-        public IActionResult DemoPage()
+        public IActionResult DemoLandingPage()
         {
             return View();
         }
